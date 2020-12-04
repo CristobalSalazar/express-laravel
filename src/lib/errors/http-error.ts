32a7858f3bdfc3279c77d23d0327a8c774bbb049
Cristobal Salazar <chris.salza@gmail.com>
2020-12-04 13:39:22 -0500

    feat; made message public

--- src/lib/errors/http-error.ts
+++ src/lib/errors/http-error.ts
@@ -1,16 +1,9 @@
 export default class HttpError {
-  public statusCode: number;
-  private message: string;
+  public statusCode: number
+  public message: string
 
   constructor(statusCode: number, message: string) {
-    this.statusCode = statusCode;
-    this.message = message;
-  }
-
-  getError() {
-    return {
-      status: this.statusCode,
-      message: this.message,
-    };
+    this.statusCode = statusCode
+    this.message = message
   }
 }
